d95399eaec6f57cfbc321c7cc299cbebb9a4bd33
zengwei <18279047775@163.com>
2018-11-19 17:58:58 +0800

    增加 工具函数js

---
+++ src/assets/js/utils.js
@@ -0,0 +1,25 @@
+//获取url后面的参数 直接从href字段上去匹配
+const getUrlParam = name => {
+  let reg = new RegExp("(^|\\?|&)" + name + "=([^&]*)(\\s|&|$)", "i");
+  if (reg.test(window.location.href)) {
+    return unescape(RegExp.$2.replace(/\+/g, " "))
+  } else {
+    return null;
+  }
+}
+
+//检测-ios终端
+const isIOS = () => {
+  return !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+}
+
+//检测-android终端
+const isAndroid = () => {
+  return navigator.userAgent.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
+}
+
+export {
+  getUrlParam,
+  isIOS,
+  isAndroid,
+}
