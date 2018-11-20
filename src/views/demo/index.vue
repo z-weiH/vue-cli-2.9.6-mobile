eb8c57304f9264eb968adde680f05a5f0f2e2369
zengwei <18279047775@163.com>
2018-11-20 10:49:51 +0800

    补充 静默授权获取 openId 逻辑

--- src/views/demo/index.vue
+++ src/views/demo/index.vue
@@ -26,6 +26,7 @@
 
 <script>
   import { Group, Cell , Popup , XButton } from 'vux'
+  import { getUrlParam } from '@/assets/js/utils'
   import qs from 'qs'
   export default {
     data() {
@@ -40,17 +41,22 @@
       XButton,
     },
     mounted() {
-      // 微信 静默授权获取 openId 逻辑 start
-      let Tencent_WxCode_api = 'https://open.weixin.qq.com/connect/oauth2/authorize?';
-      let domain_src = 'http://mobiletest.arbexpress.cn'; // 获取openId后的 回调地址
-      let loginConfig = {
-        appid: 'wx9f95c4206c9c49fc',
-        redirect_uri: domain_src + '/mobile/band/openid/query_gzh.htm',
-        response_type: 'code',
-        scope: 'snsapi_base',
-      };
-      // window.location.replace(Tencent_WxCode_api + qs.stringify(loginConfig) + '#wechat_redirect');
-      // 微信 静默授权获取 openId 逻辑 end
+      let openId = getUrlParam('code' || 'openId');
+      if(!openId) {
+        // 微信 静默授权获取 openId 逻辑 start
+        let Tencent_WxCode_api = 'https://open.weixin.qq.com/connect/oauth2/authorize?';
+        let domain_src = 'http://zw.tunnel.qydev.com'; // 获取code后的回调地址  ， 后端根据code获取openId 再做重定向操作跳转至前端url地址
+        let loginConfig = {
+          appid: 'wxb785be721e2989f3',
+          redirect_uri: domain_src + '/mobile/band/openid/query_gzh.htm',
+          response_type: 'code',
+          scope: 'snsapi_base',
+        };
+        window.location.replace(Tencent_WxCode_api + qs.stringify(loginConfig) + '#wechat_redirect');
+        // 微信 静默授权获取 openId 逻辑 end
+      }else{
+        alert(openId);
+      }
     },
     methods : {
       handleClick() {
